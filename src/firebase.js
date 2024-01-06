0da879ca0be80f4d94424d9ea09d8cbadcddd3d3
nadiamartel <nadiagmartel@gmail.com>
2024-01-06 20:02:38 -0300

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import{ getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWgYEQRowpM8jj2ww2L7HcyBmJbwzwJEs",
+    authDomain: "react-chat-5d9c8.firebaseapp.com",
+    projectId: "react-chat-5d9c8",
+    storageBucket: "react-chat-5d9c8.appspot.com",
+    messagingSenderId: "689946330239",
+    appId: "1:689946330239:web:884a85cd0174df50df2b4a"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
