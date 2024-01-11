14512bbc9b960db4f747766ff2886b6149e3e95b
nadiamartel <nadiagmartel@gmail.com>
2024-01-11 09:47:03 -0300

    fn formatDate

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,14 +1,15 @@
 import { auth } from "../firebase";
+import { formatDate } from "./utils";
 
 const Message = ({ message }) => {
 
     let style = "message"; //condicional para msj propios y de 3ros
-    if(auth.currentUser){
+    if (auth.currentUser) {
         const user = auth.currentUser.uid;
         const newUser = message.uid;
         style = user === newUser ? "my-message" : "message"
     }
-
+    console.log(message.timestamp) //esta en nanosec y sec - cambiar!
 
     return (
         <article className={style}>
@@ -16,6 +17,7 @@ const Message = ({ message }) => {
                 <div className="text-message">
                     <p className="text">{message.text}</p>
                 </div>
+                <p className="time">{formatDate(message.timestamp)}</p>
             </div>
             <img src={message.photo} alt="userPhoto" referrerPolicy="no-referrer" />
         </article>
---
+++ src/components/utils/index.js
@@ -0,0 +1,36 @@
+//  export const formatDate = (objDate) => {
+//     const date = new Date(objDate.seconds * 1000);
+
+//     const time = `${date.getHours()}:${date.getMinutes()}`;
+
+//     const modif = {
+//         month : "long",
+//         day : "numeric"
+//     }
+
+//     const newDate = date.toDateString("en-US",modif);
+//     return `${newDate} - ${time}`;
+// }
+
+//corregir el formato de hora que aparece en pantalla
+
+export const formatDate = (objDate) => {
+    if (!objDate || !objDate.seconds) {
+        return "Invalid date";
+    }
+
+    const date = new Date(objDate.seconds * 1000);
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const formattedDate = new Intl.DateTimeFormat('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+    }).format(date);
+
+    const formattedTime = `${hours}.${minutes < 10 ? '0' : ''}${minutes}`;
+
+    return `${formattedDate} - ${formattedTime}`;
+}
